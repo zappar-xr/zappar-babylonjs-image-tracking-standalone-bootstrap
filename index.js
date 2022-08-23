3898c893b1ec43e758142adb00048f31d00627dc
Zappar <hello@zappar.com>
2022-08-23 18:27:12 +0100

    Minimal project for Zappar BabylonJS image-tracking

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+
+
 // Setup BabylonJS in the usual way
 const canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
@@ -7,11 +10,11 @@ const engine = new BABYLON.Engine(canvas, true, {
     stencil: true
 });
 
-const scene = new BABYLON.Scene(engine);
+export const scene = new BABYLON.Scene(engine);
 const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
 
 // Setup a Zappar camera instead of one of Babylon's cameras
-const camera = new ZapparBabylon.Camera('camera', scene);
+export const camera = new ZapparBabylon.Camera('camera', scene);
 
 // Request the necessary permission from the user
 ZapparBabylon.permissionRequestUI().then((granted) => {
@@ -20,7 +23,9 @@ ZapparBabylon.permissionRequestUI().then((granted) => {
 });
 
 
-const imageTracker = new ZapparBabylon.ImageTrackerLoader().load("./example-tracking-image.zpt");
+const url = new URL("./example-tracking-image.zpt", import.meta.url).href;
+const imageTracker = new ZapparBabylon.ImageTrackerLoader().load(url);
+
 const trackerTransformNode = new ZapparBabylon.ImageAnchorTransformNode('tracker', camera, imageTracker, scene);
 
 // Add some content to the image tracker
@@ -35,6 +40,7 @@ imageTracker.onVisible.bind(() => {
 imageTracker.onNotVisible.bind(() => {
   box.visibility = 0;
 });
+
 
 window.addEventListener('resize', () => {
     engine.resize();
